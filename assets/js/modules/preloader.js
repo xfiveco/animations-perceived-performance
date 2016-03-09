a186e07d5de307132dd7c46047b4e334e9566c83
Lubos Kmetko <lubos@xhtmlized.com>
2016-03-09 16:18:12 +0100

    Increasing animation duration for better demonstration

--- assets/js/modules/preloader.js
+++ assets/js/modules/preloader.js
@@ -8,7 +8,7 @@ module.exports = function() {
   var loading = document.querySelector('body.is-loading');
 
   if (loading) {
-    setTimeout(function() { loading.classList.remove('is-loading'); loading.classList.add('is-loaded'); }, 2000);
+    setTimeout(function() { loading.classList.remove('is-loading'); loading.classList.add('is-loaded'); }, 4000);
   }
 
 };
