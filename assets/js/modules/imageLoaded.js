e5a430ae0f12bdd4dd12737c6eb8dfdbd98eb226
Lubos Kmetko <lubos@xhtmlized.com>
2016-03-11 13:31:11 +0100

    Inline new styles

--- assets/js/modules/imageLoaded.js
+++ assets/js/modules/imageLoaded.js
@@ -9,13 +9,20 @@ module.exports = function() {
 
   if (!image) { return; }
 
-  var img = new Image();
-  var target = document.querySelector(image.getAttribute('data-target'));
-  var imgSrc = image.getAttribute('xlink:href');
+  if (image.hasAttribute('data-target')) {
+    var img = new Image();
+    var target = document.querySelector(image.getAttribute('data-target'));
+    var imgSrc = image.getAttribute('xlink:href');
 
-  img.src = imgSrc;
+    img.src = imgSrc;
 
-  img.onload = function () {
-    target.classList.add('src-image-loaded');
-  };
+    img.onload = function () {
+      target.classList.add('src-image-loaded');
+    };
+
+  } else {
+    image.onload = function () {
+      this.classList.add('is-loaded');
+    };
+  }
 };
--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";require("./modules/imageLoaded")(),require("./modules/preloader")();
 },{"./modules/imageLoaded":2,"./modules/preloader":3}],2:[function(require,module,exports){
-"use strict";module.exports=function(){var e=document.querySelector(".js-image-load");if(e){var t=new Image,r=document.querySelector(e.getAttribute("data-target")),a=e.getAttribute("xlink:href");t.src=a,t.onload=function(){r.classList.add("src-image-loaded")}}};
+"use strict";module.exports=function(){var t=document.querySelector(".js-image-load");if(t)if(t.hasAttribute("data-target")){var e=new Image,a=document.querySelector(t.getAttribute("data-target")),d=t.getAttribute("xlink:href");e.src=d,e.onload=function(){a.classList.add("src-image-loaded")}}else t.onload=function(){this.classList.add("is-loaded")}};
 },{}],3:[function(require,module,exports){
 "use strict";module.exports=function(){var e=document.querySelector("body.is-loading");e&&setTimeout(function(){e.classList.remove("is-loading"),e.classList.add("is-loaded")},2e3)};
 },{}]},{},[1])
